4eff0fbf89338e23df9bf8641d4f70ffc08c814b
Natalia Bartolome <nzbartolome@gmail.com>
2023-05-22 21:10:17 -0300

    ejercicio 1 QA

---
+++ src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author nataliaba
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte bits8 = 4;
+        short bits16 = 18;
+        int entero32 = 60;
+        long entero64 = 100;
+        float decimal32 = 4.5f; //da error si no 'casteas'  Casting poner f al final o
+        //float decim32 = (float)4.6 // algo asi pero me da error
+        double decimal64 = decimal32; // da el doble de mi variable float
+        boolean verdaderoOfalso = true; // da error
+        char primerCaracter = 'a';
+        String cadenaNombre = "Natalia";
+        
+        // DETECCIÓN DE ERRORES
+// ¿Puedes corregir las siguientes declaraciones de variables?
+
+//string nombre
+//bolean bandera
+//char char;
+
+String nombre;
+boolean bandera;
+char caracter;
+
+}
+        
+        
+    }
+    
+
